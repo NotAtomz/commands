8b216343f0a063f4a37d0aff9871e2748e17e350
officialtubers93 <92261290+officialtubers93@users.noreply.github.com>
2022-08-12 18:42:51 -0500

    Update t-ware.lua

--- t-ware.lua
+++ t-ware.lua
@@ -7,192 +7,201 @@ local function CMSG(text)
 	});
 end
 
+function Chat(Message)
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(Message, "All")
+end
+
+
+local players = game:GetService("Players")
+local plr = players.LocalPlayer
+
+local function getChar()
+    return plr.Character
+end
+
+local function getBp()
+    return plr.Backpack
+end
+
+local function getPlr(str)
+    for i,v in pairs(players:GetPlayers()) do
+        if v.Name:lower():match(str) or v.DisplayName:lower():match(str) then
+            return v
+        end
+    end
+end
+
+local netless_Y = Vector3.new(0, 26, 0)
+local v3_101 = Vector3.new(1, 0, 1)
+local inf = math.huge
+local v3_0 = Vector3.new(0,0,0)
+local function getNetlessVelocity(realPartVelocity) --edit this if you have a better netless method
+    if (realPartVelocity.Y > 1) or (realPartVelocity.Y < -1) then
+        return realPartVelocity * (25.1 / realPartVelocity.Y)
+    end
+    realPartVelocity = realPartVelocity * v3_101
+    local mag = realPartVelocity.Magnitude
+    if mag > 1 then
+        realPartVelocity = realPartVelocity * 100 / mag
+    end
+    return realPartVelocity + netless_Y
+end
+
+local function align(Part0, Part1, p, r)
+	Part0.CustomPhysicalProperties = PhysicalProperties.new(0.0001, 0.0001, 0.0001, 0.0001, 0.0001)
+    Part0.CFrame = Part1.CFrame
+	local att0 = Instance.new("Attachment", Part0)
+	att0.Orientation = r or v3_0
+	att0.Position = v3_0
+	att0.Name = "att0_" .. Part0.Name
+	local att1 = Instance.new("Attachment", Part1)
+	att1.Orientation = v3_0 
+	att1.Position = p or v3_0
+	att1.Name = "att1_" .. Part1.Name
+
+	local apd = Instance.new("AlignPosition", att0)
+	apd.ApplyAtCenterOfMass = false
+	apd.MaxForce = inf
+	apd.MaxVelocity = inf
+	apd.ReactionForceEnabled = false
+	apd.Responsiveness = 200
+	apd.Attachment1 = att1
+	apd.Attachment0 = att0
+	apd.Name = "AlignPositionRfalse"
+	apd.RigidityEnabled = false
+
+	local ao = Instance.new("AlignOrientation", att0)
+	ao.MaxAngularVelocity = inf
+	ao.MaxTorque = inf
+	ao.PrimaryAxisOnly = false
+	ao.ReactionTorqueEnabled = false
+	ao.Responsiveness = 200
+	ao.Attachment1 = att1
+	ao.Attachment0 = att0
+	ao.RigidityEnabled = false
+    
+	if type(getNetlessVelocity) == "function" then
+	    local realVelocity = Vector3.new(0,30,0)
+        local steppedcon = game:GetService("RunService").Stepped:Connect(function()
+            Part0.Velocity = realVelocity
+        end)
+        local heartbeatcon = game:GetService("RunService").Heartbeat:Connect(function()
+            realVelocity = Part0.Velocity
+            Part0.Velocity = getNetlessVelocity(realVelocity)
+        end)
+        Part0.Destroying:Connect(function()
+            Part0 = nil
+            steppedcon:Disconnect()
+            heartbeatcon:Disconnect()
+        end)
+	end
+	
+    att0.Orientation = r or v3_0
+	att0.Position = v3_0
+	att1.Orientation = v3_0 
+	att1.Position = p or v3_0
+	Part0.CFrame = Part1.CFrame
+end
+
+local function attachTool(tool,cf)
+    for i,v in pairs(tool:GetDescendants()) do
+        if not (v:IsA("BasePart") or v:IsA("Mesh") or v:IsA("SpecialMesh")) then
+            v:Destroy()
+        end
+    end
+    local rgrip1 = Instance.new("Weld")
+	rgrip1.Name = "RightGrip"
+	rgrip1.Part0 = getChar()["Right Arm"]
+	rgrip1.Part1 = tool.Handle
+	rgrip1.C0 = cf
+	rgrip1.C1 = tool.Grip
+	rgrip1.Parent = getChar()["Right Arm"]
+    tool.Parent = getBp()
+    tool.Parent = getChar().Humanoid
+    tool.Parent = getChar()
+    tool.Handle:BreakJoints()
+    tool.Parent = getBp()
+    tool.Parent = getChar().Humanoid
+    local rgrip2 = Instance.new("Weld")
+	rgrip2.Name = "RightGrip"
+	rgrip2.Part0 = getChar()["Right Arm"]
+	rgrip2.Part1 = tool.Handle
+	rgrip2.C0 = cf
+	rgrip2.C1 = tool.Grip
+	rgrip2.Parent = getChar()["Right Arm"]
+    return rgrip2
+end
+
+local nc = false
+local ncLoop
+ncLoop = game:GetService("RunService").Stepped:Connect(function()
+	if nc and getChar() ~= nil then
+		for _, v in pairs(getChar():GetDescendants()) do
+			if v:IsA("BasePart") and v.CanCollide == true then
+				v.CanCollide = false
+			end
+		end
+    end
+end)
+
+local netsleepTargets = {}
+local nsLoop
+nsLoop = game:GetService("RunService").Stepped:Connect(function()
+    if #netsleepTargets == 0 then return end
+    for i,v in pairs(netsleepTargets) do
+        if v.Character then
+            for i,v in pairs(v.Character:GetChildren()) do
+                if v:IsA("BasePart") == false and v:IsA("Accessory") == false then continue end
+                if v:IsA("BasePart") then
+                    sethiddenproperty(v,"NetworkIsSleeping",true)
+                elseif v:IsA("Accessory") and v:FindFirstChild("Handle") then
+                    sethiddenproperty(v.Handle,"NetworkIsSleeping",true)
+                end
+            end
+        end
+    end
+end)
+
+local cc;cc = plr.Chatted:Connect(function(msg)
+    local spaceSplit = msg:split(" ")
+
 scriptcmds = {
     --// Starting with reanimations
     ["r6"] =  function()
-        CMSG(" > Reanimated!")     
+        CMSG(" > Making You R6!")     
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Reanimation/Mizt%20Reanimation.lua',true))()  
     end,
     
-    ["r6fling"] =  function()
-        CMSG(" > Reanimated with flings on!")       
+    ["touchfling"] =  function()
+        CMSG(" > Making You TouchFling Players!")       
         loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Reanimation/Mizt%20Flings.lua',true))() 
     end,
 
-    ["r6god"] =  function()
-        CMSG(" > Reanimated with godmode on!")
+    ["god"] =  function()
+        CMSG(" > Giving GodMode!")
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Reanimation/Mizt%20Perma%20Death.lua',true))()     
     end,
 	
     --// And finally converts
-    ["sun sword"] =  function()
-        CMSG(" > Running Yellow and Black Strong Sun Sword by ???")  
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Yellow%20and%20Black%20Strong%20Sun%20Sword%20Convert.lua',true))()
+    ["btools"] =  function()
+        CMSG(" > Giving You Btools!")  
+        loadstring(game:GetObjects("rbxassetid://6695644299")[1].Source)()
     end,
 
-    ["huntsman"] =  function()
-        CMSG(" > Running Phosphoria Huntsman Knife by ???")  
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Phosphoria%20Huntsman%20Knife%20Convert.lua',true))()
+    ["chatscript"] =  function()
+        CMSG(" > Chatting Script Please Wait!")  
+        wait(0.5)
+       Chat("This Player Is Using A FE Admin Script Which You Can Buy At tubers93#4764 pt")
+       Chat("Also This Script Has 92 Commands! Script Version Is 4.0.1")
+    
+    ["toolflingall"] =  function()
+        CMSG(" > Starting Tool Fling Reset Or Re To Stop!")  
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/DigitalityScripts/roblox-scripts/main/Tool%20Loop%20Fling%20All"))()
     end,
     
-    ["kitchen"] =  function()
-        CMSG(" > Running Kitchen Gun by WafflesAreVeryGood")  
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Kitchen%20Gun%20Convert.lua',true))()
-    end,
-    
-    ["nep v"] =  function()
-        CMSG(" > Running Neptune/Neptunian V by NoobyGames12")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Neptunian%20V%20Convert.lua',true))()
-    end,
-    
-    ["xester"] =  function()
-        CMSG(" > Running Xester V by tubers93")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Xester%20Convert.lua',true))()
-    end,
-    
-    ["krystal v1"] =  function()
-        CMSG(" > Running Krystal Dance v1 by The Krystal Team")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Krystal%20Dance%20V1%20Convert.lua',true))()
-    end,
-    
-    ["krystal v2"] =  function()
-        CMSG(" > Running Krystal Dance V2 by Supr14")
-        CMSG(" [also credits to krystalteam for krystal dances v1]")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Krystal%20Dances%20V2%20Convert.lua',true))()
-    end,
-    
-    ["abyss"] =  function()
-        CMSG(" > Running Abyss Eye by Shackluster")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Abyss%20Eye%20Convert.lua',true))()
-    end,
-	
-    ["abyss d"] = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Abyss%20Eye%20Demonic%20Convert.lua',true))()
-    end,		
-    
-    ["gale"] =  function()
-        CMSG(" > Running Gale Fighter by makhail07 and KillerDarkness0105")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Gale%20Fighter%20Convert.lua',true))()
-    end,
-    
-    ["ender"] =  function()
-        CMSG(" > Running Ender by makhail07")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Ender%20Convert.lua',true))()
-    end,
-    
-    ["sonic"] =  function()
-        CMSG(" > Running Sonic Script by KillerDarkness0105")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Sonic%20The%20Hedehog%20Convert.lua',true))()
-    end,
-    
-    ["memeus 2"] =  function()
-        CMSG(" > Running Memeus 2.0 by tubers93")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Memeus%202.0%20Convert.lua',true))()
-    end,
-    
-    ["memeus 2.5"] =  function()
-        CMSG(" > Running Memeus 2.5 originally by makhail07 edited by 2003boobear")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Memeus%202.5%20Convert.lua',true))()
-    end,
-    
-    ["chara"] =  function()
-        CMSG(" > Running Chara by Nebula_Zorua")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Chara%20Convert.lua',true))()
-    end,
-    
-    ["sans"] =  function()
-        CMSG(" > Running Undertale Sans by Nebula_Zorua")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Undertale%20Sans%20Convert.lua',true))()
-    end,
-    
-    ["caac"] =  function()
-        CMSG(" > Running Jump In The CAAC By EthanHong0407")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Jump%20In%20The%20CAAC%20Convert.lua',true))()
-    end,
-    
-    ["ultima"] =  function()
-        CMSG(" > Running Dual Ultima by Sugarie Saffron")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Dual%20Ultima%20Convert.lua',true))()
-    end,
-    
-    ["server"] =  function()
-        CMSG(" > Running Server Admin by Shackluster")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Server%20Admin%20Convert.lua',true))()
-    end,
-    
-    ["meme"] =  function()
-        CMSG(" > Running Meme Animations by 123jl123? [idk]")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Meme%20Animations%20Convert.lua',true))()
-    end,
-    
-    ["magic"] =  function()
-        CMSG(" > Running Magic Unknown by KillerDarkness0105")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Magic%20Unknown%20Convert.lua',true))()
-    end,
-    
-    ["sassy"] =  function()
-        CMSG(" > Running Sassy Girl by Goodguyaiden")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Sassy%20Girl%20Convert.lua',true))()
+    ["loopflingall"] =  function()
+        CMSG(" > Starting Loop Fling Reset Or Re To Stop")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/DigitalityScripts/roblox-scripts/main/loop%20fling%20all"))()
     end,
 
-    ["darkheart"] =  function()
-        CMSG(" > Running Linked Sword by SezHu")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Linked%20Sword%20Convert.lua',true))()
-    end,
-
-    ["hover"] =  function()
-        CMSG(" > Running Light HoverBoard by ???")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Hoverboard%20Convert.lua',true))()
-    end,
-
-    ["banhammer"] =  function()
-        CMSG(" > Running Ban Hammer by Creterisk")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Ban%20Hammer%20Convert.lua',true))()
-    end,
-
-    ["chips"] =  function()
-        CMSG(" > Running Chips by Creterisk")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Chips%20Convert.lua',true))()
-    end,
-
-    ["chill"] = function()
-        CMSG(" > Running Chill by ???")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Chill%20Convert.lua',true))()
-    end,
-	
-    ["nebula"] = function()
-	    CMSG(" > Running Nebula Star Glitcher by Nebula_Zorua")
-	    loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Nebula%20Star%20Glitcher%20Convert.lua',true))()
-    end,
-
-    ["hacker"] = function()
-	    CMSG(" > Running Hacker X by tubers93")
-	    loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Hacker%20X%20Paid%20Convert.lua',true))()
-    end,
-	
-     ["hacker f"] = function()
-	    CMSG(" > Running Hacker X by InfiniteOneWithdank")
-	    loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Hacker%20X%20Convert.lua',true))()
-    end,
-	
-     ["distorted"] = function()
-	    CMSG(" > Running The Distorted by Makhai07")
-	    loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/The%20Distorted%20Convert.lua',true))()
-    end,
-
-    ["voodoo"] = function()
-        CMSG(" > Running Voodoo Child by tubers93")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Voodoo%20Child%20Convert.lua',true))()
-    end,
-
-    ["voodoo f"] = function()
-        CMSG(" > Running Voodoo Child Free by Creterisk")
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Voodoo%20Child%20Free%20Convert.lua',true))()
-    end,
-	
-    ["cart"] = function()
-	CMSG(" > Running Shopping Cart by tubers93")
-	CMSG(" > *Warning, 90% of this is FD meaning no one will see you pick people up")
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/CONVERTWARE/main/convertware/Scripts/Converted/Shopping%20Cart%20Convert.lua',true))()
-    end,
 }
